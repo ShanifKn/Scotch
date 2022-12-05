f794d666e9e74ff576c6c6c022408c0ff806a619
ShanifKn <shanifkaraden06@gmail.com>
2022-12-05 07:32:35 +0530

    orderManage

---
+++ model/coupon.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+import mongoose from "mongoose";
+
+const couponSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    percentage: {
+      type: Number,
+      required: true,
+    },
+    maximum: {
+      type: Number,
+      required: true,
+    },
+    user: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const couponModel = model("coupon", couponSchema);
+
+export { couponModel };
